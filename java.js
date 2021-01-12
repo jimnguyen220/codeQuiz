0dd29ab328e3b5a214ccdcdfcfe2e413555f95eb
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-11 20:57:45 -0600

    post tutor session - created logic to increment to next question and created conditions to increment timer

--- java.js
+++ java.js
@@ -9,42 +9,49 @@ var answerButtonEl2 = document.createElement("button")
 var answerButtonEl3 = document.createElement("button")
 var answerButtonEl4 = document.createElement("button")
 
-answerButtonEl1.setAttribute("class", "btn btn-primary");
-answerButtonEl2.setAttribute("class", "btn btn-primary");
-answerButtonEl3.setAttribute("class", "btn btn-primary");
-answerButtonEl4.setAttribute("class", "btn btn-primary");
+
+answerButtonEl1.setAttribute("class", "btn btn-primary questitonBtn");
+answerButtonEl1.setAttribute("data-set", 1);
+answerButtonEl2.setAttribute("class", "btn btn-primary questitonBtn");
+answerButtonEl2.setAttribute("data-set", 2);
+answerButtonEl3.setAttribute("class", "btn btn-primary questitonBtn");
+answerButtonEl3.setAttribute("data-set", 3);
+answerButtonEl4.setAttribute("class", "btn btn-primary questitonBtn");
+answerButtonEl4.setAttribute("data-set", 4);
 
 var secondsLeft = 75;
-var score = 0;
+var index = 0;
+
+
 
 
 var quizQuestions = [
     {
-    question: "What does the alert() function do in Javascript?", 
-    answer1: "creates a popup on the page",
-    answer2: "alerts a noise to the user",
-    answer3: "calls the cops",
-    answer4: "changes the colors on the page",
-    correctAnswer: 1
+        question: "What does the alert() function do in Javascript?",
+        answer1: "creates a popup on the page",
+        answer2: "alerts a noise to the user",
+        answer3: "calls the cops",
+        answer4: "changes the colors on the page",
+        correctAnswer: 1,
     },
     {
-    question: "What does URL stand for?", 
-    answer1: "Unicorns, Rhinos and Lions",
-    answer2: "Uniform Resource Locator",
-    answer3: "Urbanization of Reformed Librarians",
-    answer4: "United Regional Lutherans",
-    correctAnswer: 2
+        question: "What does URL stand for?",
+        answer1: "Unicorns, Rhinos and Lions",
+        answer2: "Uniform Resource Locator",
+        answer3: "Urbanization of Reformed Librarians",
+        answer4: "United Regional Lutherans",
+        correctAnswer: 2,
     },
 ];
 
 console.log(quizQuestions);
 // function codes
-function setTime () {
-    var timerInterval = setInterval(function() {
+function setTime() {
+    var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = "Time remaining: " + secondsLeft;
 
-        if(secondsLeft === 0) {
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
             //insert function here to end of game
         }
@@ -68,25 +75,53 @@ function startGame() {
     quizBox(0);
 };
 
-function quizBox(index) {
+function quizBox() {
 
-        var currentQuestion = quizQuestions[index];
-     
-        questionContent.textContent = currentQuestion.question;
-        answerButtonEl1.textContent = currentQuestion.answer1
-        answerButtonEl2.textContent = currentQuestion.answer2
-        answerButtonEl3.textContent = currentQuestion.answer3
-        answerButtonEl4.textContent = currentQuestion.answer4
 
-        questionBoxEl.appendChild(questionContent);
+    var currentQuestion = quizQuestions[index];
 
-        questionBoxEl.appendChild(answerButtonEl1);
-        questionBoxEl.appendChild(answerButtonEl2);
-        questionBoxEl.appendChild(answerButtonEl3);
-        questionBoxEl.appendChild(answerButtonEl4);
+    questionContent.textContent = currentQuestion.question;
+    answerButtonEl1.textContent = currentQuestion.answer1
+    answerButtonEl2.textContent = currentQuestion.answer2
+    answerButtonEl3.textContent = currentQuestion.answer3
+    answerButtonEl4.textContent = currentQuestion.answer4
 
-       
-    };
+    questionBoxEl.appendChild(questionContent);
+
+    questionBoxEl.appendChild(answerButtonEl1);
+    questionBoxEl.appendChild(answerButtonEl2);
+    questionBoxEl.appendChild(answerButtonEl3);
+    questionBoxEl.appendChild(answerButtonEl4);
+
+    var buttonEl = document.getElementsByClassName("questitonBtn");
+
+    for (var i = 0; i < buttonEl.length; i++) {
+
+        buttonEl[i].addEventListener('click', function () {
+            var userChoice = this.getAttribute('data-set');
+         
+            userChoice = parseInt(userChoice);
+            index++;
+            if (index === quizQuestions.length+1){
+               alert("out of questions");
+            } else {
+                if (userChoice === currentQuestion.correctAnswer) {
+                    alert('correct');
+                    quizBox();
+                }  else {
+                    alert('incorrect');
+                    secondsLeft = secondsLeft - 10;
+                    quizBox();
+                }
+            }
+            
+            
+        });
+    }
+
+
+
+};
 
 
 
