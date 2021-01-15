acca3656a34bad9c9ee8e680568570a2c83db222
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-14 23:02:29 -0600

    fixed link on html and working on storing initals to local storage

--- assets/highscorejava.js
+++ assets/highscorejava.js
@@ -1,3 +1,21 @@
 var yourScore = localStorage.getItem("score");
 var highScoresEl = document.querySelector("#highscores");
-clearButton = document.querySelector("#clear");
+var scoreTable = document.querySelector("#score-table");
+var enterButton = document.querySelector("#button-addon2");
+var clearButton = document.querySelector("#clear");
+var initialsEl = document.querySelector("#three-letters");
+var initialsValue = localStorage.getItem("initials")
+
+
+
+function setInitials() {
+    localStorage.setItem("initials", initialsEl);
+}
+
+
+
+
+
+
+enterButton.addEventListener("click", setInitials);
+// clearButton.addEventListener("click", )
