d587ab7e54d66814f59edfc234cbd80d375f1d38
Jim Nguyen <jimnguyen220@gmail.com>
2021-01-14 23:14:52 -0600

    added result to score page

--- assets/highscorejava.js
+++ assets/highscorejava.js
@@ -4,16 +4,20 @@ var scoreTable = document.querySelector("#score-table");
 var enterButton = document.querySelector("#button-addon2");
 var clearButton = document.querySelector("#clear");
 var initialsEl = document.querySelector("#three-letters");
+var lastScore = document.querySelector("#last-score");
 var initialsValue = localStorage.getItem("initials")
 
 
+function showScore() {
+    lastScore.append(yourScore);
+}
 
 function setInitials() {
     localStorage.setItem("initials", initialsEl);
 }
 
 
-
+showScore();
 
 
 
